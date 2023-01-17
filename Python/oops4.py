c63f1949a61b8c81186a48b5d4b638a23879332e
Shubham Kumar Singh <98223531+Shubhamkumar-op@users.noreply.github.com>
2023-01-17 13:08:25 +0530

    Add files via upload

---
+++ Python/oops4.py
@@ -0,0 +1,28 @@
+class Employee:
+    no_of_leaves = 9
+    def __init__(self,aName,aSalary,aRole):
+        self.name=aName
+        self.salary=aSalary
+        self.role=aRole
+
+
+    def Printdetails(self):
+        return f"Name is {self.name}, Salary is {self.salary}, role is {self.role}"
+
+    @classmethod
+    def Change_leaves(cls,new_leaves):
+        cls.no_of_leaves = new_leaves
+
+    @classmethod
+    def from_str(cls,string):
+        return cls(*string.split("-"))
+
+
+shubham = Employee("Shubham", 100000000, "Captain")
+saum = Employee("saumya",1000000,"lieutenant")
+karan = Employee.from_str("karan-4555-student")
+print(karan.name)
+
+
+# saum.Change_leaves(16)
+# shubham.Change_leaves(15)
